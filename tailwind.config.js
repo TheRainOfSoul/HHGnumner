cb0dd118f9a1a7c4007e6a65e8a9052fed20ac8f
TheRainOfSoul <yaponchikhayko@gmail.com>
2025-10-01 00:32:45 +0400

    132

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+      "./index.html",
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        colors: {
+          'brand-dark': '#1A1A1A',
+          'brand-dark-2': '#2C2C2C',
+          'brand-red': '#E53E3E',
+          'brand-light': '#F7FAFC',
+          'brand-gray': '#A0AEC0',
+        },
+        fontFamily: {
+          sans: ['Inter', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
